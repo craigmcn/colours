591c23e738b126505fe0d5981aa19b8f7f7fe74d
Craig McNaughton <therealcraigmcn@gmail.com>
2020-05-03 12:18:14 -0700

    Fix hsl parser

--- src/scripts/parseValues.js
+++ src/scripts/parseValues.js
@@ -1,25 +1,25 @@
-import { hex2Rgb, rgb2Hsl, rgb2Hex } from './convertColours'
+import { hex2Rgb, rgb2Hsl, rgb2Hex, hsl2Rgb } from './convertColours'
 
-export const splitHex = hex => {
+export const splitHex = (hex) => {
   if (!hex) return ['00', '00', '00']
   const h = hex.replace('#', '').toLowerCase()
   if (!h.match(/^[a-f0-9]{3}$/) && !h.match(/^[a-f0-9]{6}$/))
     return ['00', '00', '00']
   if (h.length === 3) {
-    return h.split('').map(c => c + c)
+    return h.split('').map((c) => c + c)
   } else {
     const a = h.match(/^(.{2})(.{2})(.{2})$/)
     return [a[1], a[2], a[3]]
   }
 }
 
-export const splitRgb = rgb => {
+export const splitRgb = (rgb) => {
   if (!rgb) return [0, 0, 0]
   const m = rgb.match(/^rgb\((\d{1,3}),[\s]?(\d{1,3}),[\s]?(\d{1,3})\)$/)
   return [m[1], m[2], m[3]]
 }
 
-export const splitHsl = hsl => {
+export const splitHsl = (hsl) => {
   if (!hsl) return [0, '0%', '0%']
   const m = hsl.match(/^hsl\((\d{1,3}),[\s]?(\d{1,3})%,[\s]?(\d{1,3})%\)$/)
   return [m[1], m[2], m[3]]
