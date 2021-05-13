bf8304ad0af500f9b9ad3d8eaa12de118d72e8e2
Craig McNaughton <therealcraigmcn@gmail.com>
2021-05-13 14:37:38 -0700

    Rebuild after dependabot merges
    - update to v2.0.5
    - run npm outdated, update, audit fix
    - update gulpfile for gulp-rev-rewrite
    - replace FontAwesome kit with inline SVG

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const { readFileSync } = require('fs')
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
 const flatten = require('gulp-flatten')
@@ -8,88 +9,87 @@ const rewrite = require('gulp-rev-rewrite')
 const argv = require('minimist')(process.argv.slice(2))
 const env = argv.env ? argv.env : 'development'
 const output = {
-  development: './tmp',
-  production: './prod',
+    development: './tmp',
+    production: './prod',
 }
 const browserSync = require('browser-sync').create()
 
 // CSS
 const sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer')
+    autoprefixer = require('gulp-autoprefixer')
 
 const sassOptions = {
-  development: {
-    errLogToConsole: true,
-    outputStyle: 'expanded',
-  },
-  production: {
-    errLogToConsole: false,
-    outputStyle: 'compressed',
-  },
+    development: {
+        errLogToConsole: true,
+        outputStyle: 'expanded',
+    },
+    production: {
+        errLogToConsole: false,
+        outputStyle: 'compressed',
+    },
 }
 
 gulp.task('styles', () => {
-  return gulp
-    .src('./src/styles/**/*.scss')
-    .pipe(sass(sassOptions[env]).on('error', sass.logError))
-    .pipe(autoprefixer({ grid: 'autoplace' }))
-    .pipe(flatten())
-    .pipe(hash(hashOptions))
-    .pipe(gulp.dest(output[env]))
-    .pipe(
-      hash.manifest(hashFilename, {
-        deleteOld: true,
-        sourceDir: __dirname + output[env].substring(1),
-      })
-    )
-    .pipe(gulp.dest(output[env]))
+    return gulp
+        .src('./src/styles/**/*.scss')
+        .pipe(sass(sassOptions[env]).on('error', sass.logError))
+        .pipe(autoprefixer({ grid: 'autoplace' }))
+        .pipe(flatten())
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+            })
+        )
+        .pipe(gulp.dest(output[env]))
 })
 
 // JS
 const browserify = require('browserify'),
-  babelify = require('babelify'),
-  source = require('vinyl-source-stream'),
-  buffer = require('vinyl-buffer'),
-  uglify = require('gulp-uglify')
+    babelify = require('babelify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify')
 
 gulp.task('scripts', () => {
-  const b = browserify({
-    entries: `./src/scripts/scripts.js`,
-    debug: false,
-  })
+    const b = browserify({
+        entries: `./src/scripts/scripts.js`,
+        debug: false,
+    })
 
-  return b
-    .transform(
-      babelify.configure({
-        presets: ['@babel/preset-env'],
-        sourceMaps: env === 'production',
-      })
-    )
-    .bundle()
-    .pipe(source('scripts.js'))
-    .pipe(gulpif(env === 'production', buffer()))
-    .pipe(gulpif(env === 'production', uglify()))
-    .pipe(flatten())
-    .pipe(hash(hashOptions))
-    .pipe(gulp.dest(output[env]))
-    .pipe(
-      hash.manifest(hashFilename, {
-        deleteOld: true,
-        sourceDir: __dirname + output[env].substring(1),
-        append: true,
-      })
-    )
-    .pipe(gulp.dest(output[env]))
+    return b
+        .transform(
+            babelify.configure({
+                presets: ['@babel/preset-env'],
+                sourceMaps: env === 'production',
+            })
+        )
+        .bundle()
+        .pipe(source('scripts.js'))
+        .pipe(gulpif(env === 'production', buffer()))
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(flatten())
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+                append: true,
+            })
+        )
+        .pipe(gulp.dest(output[env]))
 })
 
 // HTML
 gulp.task('html', done => {
-  const manifest = gulp.src(`${output[env]}/${hashFilename}`)
-  gulp
-    .src('./src/**/*.html')
-    .pipe(rewrite({ manifest }))
-    .pipe(gulp.dest(output[env]))
-  done()
+    const manifest = readFileSync(`${output[env]}/${hashFilename}`)
+    gulp.src('./src/**/*.html')
+        .pipe(rewrite({ manifest }))
+        .pipe(gulp.dest(output[env]))
+    done()
 })
 
 // Build
@@ -97,21 +97,21 @@ gulp.task('build', gulp.series('styles', 'scripts', 'html'))
 
 // Reload browser
 gulp.task('reload', done => {
-  browserSync.reload()
-  done()
+    browserSync.reload()
+    done()
 })
 
 // Browser sync
 gulp.task('browserSync', () => {
-  browserSync.init({
-    port: 1235,
-    server: output[env],
-    ui: false,
-  })
-  gulp.watch(
-    ['src/styles/**/*.scss', 'src/scripts/**/*.js', 'src/**/*.html'],
-    gulp.series('build', 'reload')
-  )
+    browserSync.init({
+        port: 1235,
+        server: output[env],
+        ui: false,
+    })
+    gulp.watch(
+        ['src/styles/**/*.scss', 'src/scripts/**/*.js', 'src/**/*.html'],
+        gulp.series('build', 'reload')
+    )
 })
 
 // Dev server
