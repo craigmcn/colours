397188b875ef9c136dbecee0fce1fb1864ca5fac
Craig McNaughton <therealcraigmcn@gmail.com>
2021-08-18 11:43:35 -0700

    Update build for Netlify
    - implement Nunjucks templates for common layout
    - update gulpfile to also write to /colours subdirectory for Netlify
    - npm update, outdated + update, audit fix

--- gulpfile.js
+++ gulpfile.js
@@ -9,109 +9,123 @@ const rewrite = require('gulp-rev-rewrite')
 const argv = require('minimist')(process.argv.slice(2))
 const env = argv.env ? argv.env : 'development'
 const output = {
-    development: './tmp',
-    production: './prod',
+  development: './tmp',
+  production: './dist',
+  netlify: './netlify',
 }
+const outputNetlify = `${output[env]}/colours`
 const browserSync = require('browser-sync').create()
 
 // CSS
-const sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer')
+const sass = require('gulp-sass')(require('sass')),
+  autoprefixer = require('gulp-autoprefixer')
 
 const sassOptions = {
-    development: {
-        errLogToConsole: true,
-        outputStyle: 'expanded',
-    },
-    production: {
-        errLogToConsole: false,
-        outputStyle: 'compressed',
-    },
+  development: {
+    errLogToConsole: true,
+    outputStyle: 'expanded',
+  },
+  production: {
+    errLogToConsole: false,
+    outputStyle: 'compressed',
+  },
 }
 
 gulp.task('styles', () => {
-    return gulp
-        .src('./src/styles/**/*.scss')
-        .pipe(sass(sassOptions[env]).on('error', sass.logError))
-        .pipe(autoprefixer({ grid: 'autoplace' }))
-        .pipe(flatten())
-        .pipe(hash(hashOptions))
-        .pipe(gulp.dest(output[env]))
-        .pipe(
-            hash.manifest(hashFilename, {
-                deleteOld: true,
-                sourceDir: __dirname + output[env].substring(1),
-            })
-        )
-        .pipe(gulp.dest(output[env]))
+  return gulp
+    .src('./src/styles/**/*.scss')
+    .pipe(sass(sassOptions[env]).on('error', sass.logError))
+    .pipe(autoprefixer({ grid: 'autoplace' }))
+    .pipe(flatten())
+    .pipe(hash(hashOptions))
+    .pipe(gulp.dest(output[env]))
+    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+    .pipe(
+      hash.manifest(hashFilename, {
+        deleteOld: true,
+        sourceDir: __dirname + output[env].substring(1),
+      })
+    )
+    .pipe(gulp.dest(output[env]))
+    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
 })
 
 // JS
 const browserify = require('browserify'),
-    babelify = require('babelify'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    uglify = require('gulp-uglify')
+  babelify = require('babelify'),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+  uglify = require('gulp-uglify')
 
 gulp.task('scripts', () => {
-    const b = browserify({
-        entries: `./src/scripts/scripts.js`,
-        debug: false,
-    })
+  const b = browserify({
+    entries: `./src/scripts/scripts.js`,
+    debug: env !== 'development',
+  })
 
-    return b
-        .transform(
-            babelify.configure({
-                presets: ['@babel/preset-env'],
-                sourceMaps: env === 'production',
-            })
-        )
-        .bundle()
-        .pipe(source('scripts.js'))
-        .pipe(gulpif(env === 'production', buffer()))
-        .pipe(gulpif(env === 'production', uglify()))
-        .pipe(flatten())
-        .pipe(hash(hashOptions))
-        .pipe(gulp.dest(output[env]))
-        .pipe(
-            hash.manifest(hashFilename, {
-                deleteOld: true,
-                sourceDir: __dirname + output[env].substring(1),
-                append: true,
-            })
-        )
-        .pipe(gulp.dest(output[env]))
+  return b
+    .transform(
+      babelify.configure({
+        presets: ['@babel/preset-env'],
+        sourceMaps: env !== 'development',
+      })
+    )
+    .bundle()
+    .pipe(source('scripts.js'))
+    .pipe(gulpif(env !== 'development', buffer()))
+    .pipe(gulpif(env !== 'development', uglify()))
+    .pipe(flatten())
+    .pipe(hash(hashOptions))
+    .pipe(gulp.dest(output[env]))
+    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+    .pipe(
+      hash.manifest(hashFilename, {
+        deleteOld: true,
+        sourceDir: __dirname + output[env].substring(1),
+        append: true,
+      })
+    )
+    .pipe(gulp.dest(output[env]))
+    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
 })
 
 // HTML
-gulp.task('html', done => {
-    const manifest = readFileSync(`${output[env]}/${hashFilename}`)
-    gulp.src('./src/**/*.html')
-        .pipe(rewrite({ manifest }))
-        .pipe(gulp.dest(output[env]))
-    done()
+gulp.task('html', () => {
+  const render = require('gulp-nunjucks-render')
+  const manifest = readFileSync(`${output[env]}/${hashFilename}`)
+
+  return gulp
+    .src('./src/pages/**/*.html')
+    .pipe(
+      render({
+        path: ['src/templates'],
+      })
+    )
+    .pipe(rewrite({ manifest }))
+    .pipe(gulp.dest(output[env]))
+    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
 })
 
 // Build
 gulp.task('build', gulp.series('styles', 'scripts', 'html'))
 
 // Reload browser
-gulp.task('reload', done => {
-    browserSync.reload()
-    done()
+gulp.task('reload', (done) => {
+  browserSync.reload()
+  done()
 })
 
 // Browser sync
 gulp.task('browserSync', () => {
-    browserSync.init({
-        port: 1235,
-        server: output[env],
-        ui: false,
-    })
-    gulp.watch(
-        ['src/styles/**/*.scss', 'src/scripts/**/*.js', 'src/**/*.html'],
-        gulp.series('build', 'reload')
-    )
+  browserSync.init({
+    port: 1235,
+    server: output[env],
+    ui: false,
+  })
+  gulp.watch(
+    ['src/styles/**/*.scss', 'src/scripts/**/*.js', 'src/**/*.html'],
+    gulp.series('build', 'reload')
+  )
 })
 
 // Dev server
