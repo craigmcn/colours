d272cfdf31d61ae54e33fd25f53c618727559c5a
Craig McNaughton <therealcraigmcn@gmail.com>
2022-01-03 13:09:38 -0800

    Patch to version 2.4.2
    - npm update, audit fix
    - add .nvmrc file
    - update header for AlbertCSS menu toggle

--- gulpfile.js
+++ gulpfile.js
@@ -129,7 +129,7 @@ gulp.task('reload', (done) => {
 // Browser sync
 gulp.task('browserSync', () => {
     browserSync.init({
-        port: 1235,
+        port: 3060,
         server: output[env],
         ui: false,
     })
